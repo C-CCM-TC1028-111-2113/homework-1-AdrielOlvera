dc827e5aea87d444b2ccd2b562af5e66afc80b3f
AdrielOlvera <82849600+AdrielOlvera@users.noreply.github.com>
2021-08-18 12:15:44 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+x1=input("Dame x1:")
+y1=input("Dame y1:")
+x2=input("Dame x2:")
+y2=input("Dame y2:")
+m=(float(y2)-float(y1))/(float(x2)-float(x1))
+print("Pendiente:"+str(m))
+
     #Lee los datos
     pass
 
