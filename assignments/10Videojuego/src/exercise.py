d6aaec6c858197bbf8bd7d01a223df234cc84715
AdrielOlvera <82849600+AdrielOlvera@users.noreply.github.com>
2021-08-18 16:03:24 -0500

    Update exercise.py

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+    new=input("Dame la cantidad de juegos nuevos: ")
+used=input("Dame la cantidad de juegos usados: ")
+total=(int(new)*1000)+(int(used)*350)
+print("El total de la compra es: "+str(int(total)))
    pass
 
 
